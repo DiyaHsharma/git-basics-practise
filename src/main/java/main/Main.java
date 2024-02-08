003b03a8f4c6fcd5db34fb9371d56473326a0cbf
DiyaHsharma <diyaabhisharma@gmail.com>
2024-02-08 11:05:19 +0530

    create main.java for writing code

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,6 @@
+package main;
+//use the following command to add this file to git: "git add main.java"
+//to add all the files in a project to git, use: "git add *"
+// use "ctrl k" to save ab tak ka project progress to git
+public class Main {
+}
